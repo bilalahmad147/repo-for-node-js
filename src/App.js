f2192d540e3b60c86c1b96c4fbffc7b3317e8bc8
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-15 04:36:23 +0500

    fetch total data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React from 'react'
-import './App.css';
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-import { useRef, useLayoutEffect } from 'react'
-am4core.useTheme(am4themes_animated);
+import GreeceTotal from './components/data/js/greeceTotal'
 
-function App(props) {
-  const chart = useRef(null);
-
-  useLayoutEffect(() => {
-    let x = am4core.create("chartdiv", am4charts.XYChart);
-
-    // ...
-
-    chart.current = x;
-
-    return () => {
-      x.dispose();
-    };
-  }, []);
-
-  // When the paddingRight prop changes it will update the chart
-  useLayoutEffect(() => {
-    chart.current.paddingRight = props.paddingRight;
-  }, [props.paddingRight]);
-
-  return (
-    <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
-  );
+function App() {
+  return(
+    <div>
+      <h1>hello world</h1>
+      <GreeceTotal />
+    </div>
+  )
 }
-
+ 
 export default App;
