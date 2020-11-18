be6f377e45a22b96fc1b9fa91b80e8a823eecc81
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-19 00:14:37 +0500

    updated app.js

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 const fs = require('fs')
-fs.writeFileSync('notes.txt','hello universe from app.js file')
+fs.writeFileSync('notes.txt','hello universe from app.js file ')
+fs.appendFileSync('notes.txt',' hello nijah from joyia')
