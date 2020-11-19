8824f68ff4911739ca7ec2f49975d743c4d6e724
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-20 02:50:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+const validator = require('validator')
 const notes = require('./notes')
 
 const msg = notes()
 
 console.log(msg)
+
+console.log(validator.isEmail('bilal@gmail.com'))
+console.log(validator.isURL('http://firebase.com'))
+
+console.log(validator.isEmail('bilalgmail.com'))
+console.log(validator.isURL('http:/firebase.com'))
