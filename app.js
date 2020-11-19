0b7bf76582a4cebc728a901172cd882fa264e65a
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-20 03:24:43 +0500

    try chalk library

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const validator = require('validator')
+const chalk = require('chalk')
 const notes = require('./notes')
 
 const msg = notes()
 
 console.log(msg)
 
-console.log(validator.isEmail('bilal@gmail.com'))
-console.log(validator.isURL('http://firebase.com'))
-
-console.log(validator.isEmail('bilalgmail.com'))
-console.log(validator.isURL('http:/firebase.com'))
+console.log(chalk.green('Success..'))
+console.log(chalk.green.bold('Hello world!'));
+console.log(chalk.blue.bgRed.bold('Hello world!'));
